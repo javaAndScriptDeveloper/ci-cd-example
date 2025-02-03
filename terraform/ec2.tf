bf5ea28266d99342eed720439cc26f6a866bd72b
vampir <fkirill440@gmail.com>
2025-02-04 00:29:56 +0200

    Fix terraform.yml

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,26 +1,6 @@
-resource "aws_instance" "my_ec2" {
-  ami                    = "ami-087fba4aa07ebd20f"
-  instance_type           = "t3.micro"
-
-  security_groups = ["allow_ssh_docker"]
-
-  tags = {
-    Name = "Docker EC2 Instance"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install -y docker.io
-              sudo systemctl enable docker
-              sudo systemctl start docker
-              sudo docker pull your-docker-image:latest
-              sudo docker run -d --name my_container your-docker-image:latest
-              EOF
-}
-
 resource "aws_security_group" "allow_ssh_docker" {
   name_prefix = "allow_ssh_docker_"
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -41,4 +21,26 @@ resource "aws_security_group" "allow_ssh_docker" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-087fba4aa07ebd20f"
+  instance_type           = "t3.micro"
+
+  # Use the security group ID, not the name
+  security_groups = [aws_security_group.allow_ssh_docker.id]
+
+  tags = {
+    Name = "Docker EC2 Instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y docker.io
+              sudo systemctl enable docker
+              sudo systemctl start docker
+              sudo docker pull your-docker-image:latest
+              sudo docker run -d --name my_container your-docker-image:latest
+              EOF
 }
