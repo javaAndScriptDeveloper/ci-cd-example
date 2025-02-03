05523acd1655e4b133ae8532b4d097b321d29437
vampir <fkirill440@gmail.com>
2025-02-04 00:49:48 +0200

    Fix terraform.yml

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -25,10 +25,8 @@ resource "aws_security_group" "allow_ssh_docker" {
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-087fba4aa07ebd20f"
   instance_type           = "t3.micro"
-  name = "single instance"
 
-  # Use the security group ID, not the name
-  security_groups = [aws_security_group.allow_ssh_docker.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_docker.id]
 
   tags = {
     Name = "Docker EC2 Instance"
